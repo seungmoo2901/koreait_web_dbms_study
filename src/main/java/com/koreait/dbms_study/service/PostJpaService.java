d7963700d1ec76ab1a8e733c389521582aaabbb1
seungmoo2901 <100752628+seungmoo2901@users.noreply.github.com>
2025-06-27 13:30:37 +0900

    Feat:JPA

--- src/main/java/com/koreait/dbms_study/service/PostJpaService.java
+++ src/main/java/com/koreait/dbms_study/service/PostJpaService.java
@@ -1,14 +1,17 @@
 package com.koreait.dbms_study.service;
 
-import com.koreait.dbms_study.dto.AddPostJpaReqDto;
 import com.koreait.dbms_study.dto.AddPostReqDto;
+import com.koreait.dbms_study.dto.ApiRespDto;
+import com.koreait.dbms_study.dto.EditPostReqDto;
 import com.koreait.dbms_study.entity.JpaPost;
 import com.koreait.dbms_study.entity.Post;
 import com.koreait.dbms_study.repository.PostJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PostJpaService {
@@ -16,10 +19,41 @@ public class PostJpaService {
     @Autowired
     private PostJpaRepository postJpaRepository;
 
-    public JpaPost addPost(AddPostJpaReqDto addPostJpaReqDto){
-        return postJpaRepository.save(addPostJpaReqDto.toJpaEntity());
+    public JpaPost addPost(AddPostReqDto addPostReqDto) {
+        return postJpaRepository.save(addPostReqDto.toJpaEntity());
     }
-    public List<JpaPost> getPostList(){
+
+    public List<JpaPost> getPostList() {
         return postJpaRepository.findAll();
+    }
+
+    public ApiRespDto<?> editPost(EditPostReqDto editPostReqDto) {
+        Optional<JpaPost> optionalPost = postJpaRepository.findById(editPostReqDto.getPostId());
+        if(optionalPost.isEmpty()) {
+            return new ApiRespDto<>("해당 게시물이 존재하지 않습니다.", null);
+        }
+        try {
+            JpaPost jpaPost = optionalPost.get();
+            jpaPost.setTitle(editPostReqDto.getTitle());
+            jpaPost.setContent(editPostReqDto.getContent());
+            jpaPost.setUpdateDt(LocalDateTime.now());
+            postJpaRepository.save(jpaPost);
+            return new ApiRespDto<>("수정 완료", jpaPost);
+        } catch (Exception e) {
+            return new ApiRespDto<>("문제가 발생했습니다.", e.getMessage());
+        }
+    }
+
+    public ApiRespDto<?> removePost(Integer postId) {
+        Optional<JpaPost> optionalPost = postJpaRepository.findById(postId);
+        if(optionalPost.isEmpty()) {
+            return new ApiRespDto<>("해당 게시물이 존재하지 않습니다.", null);
+        }
+        try {
+            postJpaRepository.deleteById(postId);
+        } catch (Exception e) {
+            return new ApiRespDto<>("문제가 발생했습니다.", e.getMessage());
+        }
+        return new ApiRespDto<>("삭제 성공", null);
     }
 }
